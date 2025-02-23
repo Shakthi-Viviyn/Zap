54d23b1f955c1ddb9b681643dd02a51b0dc14d9d
Harsh <harshpatel1632@gmail.com>
2025-02-23 05:42:12 -0500

    Add back createTransaction function

--- backend/transactions.js
+++ backend/transactions.js
@@ -1,5 +1,48 @@
-import { splitWallet } from "./wallet.js";
 import supabase from "./database/client.js";
+import { getWallets, splitWallet, fetchWalletBalances, getWalletsForTransfer } from "./wallet.js";
+
+export async function createTransaction(senderId, receiverId, targetAmount) {
+
+    let walletIds = await getWallets(senderId);
+    let { wallets: walletsWithAmount, totalAmount } = await fetchWalletBalances(walletIds);
+    console.log(walletsWithBalance);
+
+    if (totalAmount < targetAmount) {
+        throw new Error(`Insufficient funds in user account.`);
+    }
+
+    let { wallets, walletSum } = getWalletsForTransfer(walletsWithAmount, targetAmount);
+    let difference = walletSum - targetAmount;
+
+    let transaction_info;
+    if (difference > 0) {
+        let walletToSplit = wallets.pop();
+        console.log("Wallet to split: ", walletToSplit);
+        transaction_info = {
+            "walletToSplit": walletToSplit,
+            "difference": difference,
+            "wallets_data": wallets,
+            "sender_user_id": senderId,
+            "receiver_user_id": receiverId,
+            "transaction_amount": targetAmount
+        }
+    }else{
+        transaction_info = {
+            "walletToSplit": "",
+            "difference": difference,
+            "wallets_data": wallets,
+            "sender_user_id": senderId,
+            "receiver_user_id": receiverId,
+            "transaction_amount": targetAmount
+        }
+    }
+
+    const {data, error} = await supabase.from("transaction").insert({transaction_info: transaction_info}).select("id");
+    if (error) {
+        throw new Error(`Error occured: ${error.message}`);
+    }
+    return data[0].id;
+}
 
 export async function commitTransaction(transactionId){
 
